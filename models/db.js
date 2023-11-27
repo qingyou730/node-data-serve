59f5b070de18d880c011906222b460717c100ece
qingyou <qingyou@192.168.0.101>
2023-11-27 20:08:28 +0800

    fix name db

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { sqlLogger } = require("../logger");
-const sequelize = new Sequelize("globalata", "root", "", {
+const sequelize = new Sequelize("globalData", "root", "", {
   host: "localhost",
   dialect: "mysql",
   logging: (msg) => {
